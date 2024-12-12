90d8fffdb213ed78f4951c60f8892c7075c517f0
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2024-12-13 00:15:13 +0100

    format

--- day2/app/Main.hs
+++ day2/app/Main.hs
@@ -11,16 +11,16 @@ solve part = part . parse
 
 isSafe :: [Int] -> Bool
 isSafe xs = all (\x -> x > 0 && x < 4) deltas || all (\x -> x < 0 && x > -4) deltas
-  where
-    deltas = zipWith (-) xs (tail xs)
+ where
+  deltas = zipWith (-) xs (tail xs)
 
 part1 :: [[Int]] -> Int
 part1 = foldl (\acc x -> if x then acc + 1 else acc) 0 . map (\xs -> isSafe xs)
 
 isSafe2 :: [Int] -> Bool
 isSafe2 xs = elem True $ map (\x -> isSafe x) xss
-  where
-    xss = zipWith (++) (inits xs) (tail (tails xs) ++ [[]])
+ where
+  xss = zipWith (++) (inits xs) (tail (tails xs) ++ [[]])
 
 part2 :: [[Int]] -> Int
 part2 = foldl (\acc x -> if x then acc + 1 else acc) 0 . map (\xs -> isSafe2 xs)
@@ -32,7 +32,7 @@ getPart _ = error "Incorrect part"
 
 main :: IO ()
 main = do
-    args <- getArgs
-    contents <- getContents
-    let part = getPart args
-    print $ solve part contents
+  args <- getArgs
+  contents <- getContents
+  let part = getPart args
+  print $ solve part contents
