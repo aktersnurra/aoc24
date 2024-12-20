3d597cf96398e59e836c44693f245b3bbe5f7f78
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2024-12-20 23:40:47 +0100

    Add day3 part 2

--- day3/app/Main.hs
+++ day3/app/Main.hs
@@ -1,14 +1,10 @@
 module Main where
 
-import Data.List
 import System.Environment (getArgs)
 import Text.Regex.TDFA
 
-parse :: String -> [String]
-parse = filter (not . null) . lines
-
-solve :: ([String] -> Int) -> String -> Int
-solve part = part . parse
+solve :: (String -> Int) -> String -> Int
+solve part = part
 
 muls :: String -> [String]
 muls x = getAllTextMatches (x =~ "mul\\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\\)")
@@ -18,16 +14,30 @@ nums x = getAllTextMatches (x =~ "[0-9][0-9]?[0-9]?")
 
 multiply :: [String] -> Int
 multiply (x : y : []) = (*) (read x) (read y)
+multiply _ = error "err"
 
-part1 :: [String] -> Int
-part1 = sum . map sum . map (map multiply) . map (map nums) . map muls
+part1 :: String -> Int
+part1 = sum . (map multiply) . (map nums) . muls
 
--- part2 :: [[Int]] -> Int
--- part2 = foldl (\acc x -> if x then acc + 1 else acc) 0 . map (\xs -> isSafe2 xs)
+instructions :: String -> [String]
+instructions x = getAllTextMatches (x =~ "do|don\'t|mul\\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\\)")
 
-getPart :: [String] -> ([String] -> Int)
+multiply2 :: (Bool, Int) -> String -> (Bool, Int)
+multiply2 (True, b) ('m' : 'u' : 'l' : y) = (True, total)
+ where
+  mul = multiply $ nums y
+  total = b + mul
+multiply2 (False, b) ('m' : 'u' : 'l' : _) = (False, b)
+multiply2 (_, b) ['d', 'o'] = (True, b)
+multiply2 (_, b) ['d', 'o', 'n', '\'', 't'] = (False, b)
+multiply2 (a, b) _ = (a, b)
+
+part2 :: String -> Int
+part2 = snd . foldl multiply2 (True, 0) . instructions
+
+getPart :: [String] -> (String -> Int)
 getPart ["1"] = part1
--- getPart ["2"] = part2
+getPart ["2"] = part2
 getPart _ = error "Incorrect part"
 
 main :: IO ()
@@ -35,4 +45,4 @@ main = do
   args <- getArgs
   contents <- getContents
   let part = getPart args
-  print $ solve part contents
+  print $ part contents
